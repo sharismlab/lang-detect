2f9c1189d4168d2df59042eb3c9eac49b7aadd8c
BrainGNP <braingnp@gmail.com>
2011-07-20 07:05:04 +0800

    mv meta to prepare

--- meta/fetch.py
+++
@@ -1,4 +0,0 @@
-#
-# Fetch the feature articles in a language version of Wikipedia as the corpus
-# for analysis.
-#
---
+++ prepare/fetch.py
@@ -0,0 +1,20 @@
+#
+# Fetch the feature articles in a language version of Wikipedia as the corpus
+# for analysis.
+#
+
+fa = {}
+fa['de'] = 'Wikipedia:Exzellente_Artikel'
+fa['en'] = 'Wikipedia:Featured_articles'
+fa['es'] = 'Wikipedia:Artículos_destacados'
+fa['fr'] = 'Wikipédia:Articles de qualité'
+fa['it'] = 'Wikipedia:Vetrina'
+fa['ja'] = 'Wikipedia:秀逸な記事'
+fa['nl'] = 'Wikipedia:Etalage'
+fa['pl'] = 'Wikipedia:Artykuły na medal'
+fa['ru'] = 'Википедия:Избранные статьи'
+fa['zh_hans'] = '维基百科:特色条目'
+fa['zh_hant'] = '維基百科:特色條目'
+fa['zh_yue'] = 'Wikipedia:正文'
+
+
